ffd27cc423adbe5410bdb089e719725b0a42f8a4
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-06-05 22:20:35 -0300

    develop: Criação do projeto base com Gradle e Java 11

---
+++ src/main/java/com/lucasbarbosa/libraryapi/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.lucasbarbosa.libraryapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/books")
+public class BookController {
+
+    @GetMapping
+    public String fetchBooks(){
+        return "Fetching books ...";
+    }
+}
