59156086d2c8be540daeb7c0a115fd6da6ae6cfb
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-07-24 00:27:00 -0300

    develop: adding Swagger docs to SellerRequestDTO

--- src/main/java/com/lucasbarbosa/libraryapi/model/dto/SellerRequestDTO.java
+++ src/main/java/com/lucasbarbosa/libraryapi/model/dto/SellerRequestDTO.java
@@ -3,6 +3,8 @@ package com.lucasbarbosa.libraryapi.model.dto;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.lucasbarbosa.libraryapi.driver.validation.SellerAssurance;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -13,16 +15,22 @@ import lombok.Getter;
 @Builder
 @SellerAssurance
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@ApiModel(value = "SellerRequestDTO", description = "Data transfer object for sellers registration and token acquirence")
 public class SellerRequestDTO {
 
+    @ApiModelProperty(value = "Seller CPF", example = "021.423.543-65")
     private String cpf;
 
+    @ApiModelProperty(value = "Seller company CNPJ", example = "72.871.272/0001-78")
     private String cnpj;
 
+    @ApiModelProperty(value = "Seller company name", example = "Cyberpunk SA")
     private String companyName;
 
+    @ApiModelProperty(value = "Seller full name", example = "Tim Peterson Morales")
     private String personName;
 
+    @ApiModelProperty(value = "Seller license type", example = "INDIVIDUAL", allowableValues = "INDIVIDUAL, COMPANY")
     private String licenseType;
 
 }
