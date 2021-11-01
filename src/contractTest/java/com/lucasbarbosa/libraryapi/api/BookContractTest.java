3d1095287b2fc3b4870c653b7fa6655539e8018c
Lucas Barbosa <lucasbarbosa1738@gmail.com>
2021-10-31 21:09:15 -0300

    creating contractTest task in build.gradle

--- src/contractTest/java/com/lucasbarbosa/libraryapi/api/BookContractTest.java
+++ src/contractTest/java/com/lucasbarbosa/libraryapi/api/BookContractTest.java
@@ -1,21 +1,25 @@
-package com.lucasbarbosa.libraryapi.controller;
+package com.lucasbarbosa.libraryapi.api;
 
+import com.lucasbarbosa.libraryapi.ContractTestConfiguration;
 import com.lucasbarbosa.libraryapi.template.BookRequestTemplate;
+import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 /** @author Lucas Barbosa on 03/07/2021 */
+@ContextConfiguration(classes = {ContractTestConfiguration.class})
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@WebMvcTest
 public class BookContractTest extends BookContractTestSupport {
 
   @Test
   @DisplayName(
       "Given unsuitable bookGenre value, then return BAD REQUEST with field validation message")
-  public void givenBookGenrerUnallowedValueAndExcessiveNumberPagesThenReturnBadRequest() throws Exception {
+  public void givenBookGenrerUnallowedValueAndExcessiveNumberPagesThenReturnBadRequest()
+      throws Exception {
 
     writeAsJson(BookRequestTemplate.buildWithUnsuitableBookGenreAndExcessiveNumberPages());
 
@@ -37,7 +41,7 @@ public class BookContractTest extends BookContractTestSupport {
   @Test
   @DisplayName(
       "Given zero number of pages and empty title, then return BAD REQUEST with field validation message")
-  void givenZeroNumberOfPagesAndEmptyTitleThenReturnBadRequest() throws Exception {
+  public void givenZeroNumberOfPagesAndEmptyTitleThenReturnBadRequest() throws Exception {
 
     writeAsJson(BookRequestTemplate.buildWithZeroNumberPagesAndEmptyTitle());
 
---
+++ src/contractTest/java/com/lucasbarbosa/libraryapi/api/BookContractTestSupport.java
@@ -0,0 +1,56 @@
+package com.lucasbarbosa.libraryapi.api;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+import static org.junit.Assert.assertTrue;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/** @author Lucas Barbosa on 03/07/2021 */
+public class BookContractTestSupport {
+
+  @Autowired private MockMvc mockMvc;
+
+  protected String json;
+  protected String BOOK_API = "/books";
+  protected String CREATE_BOOK = "/register/";
+  protected String MESSAGE_FIELD = "$.message";
+  protected String BOOK_GENRE_ENUM_VALIDATION_MESSAGE =
+      "field bookGenre must be any of ACTION, COMEDY, DRAMA, FANTASY, HORROR, MYSTERY, ROMANCE, THRILLER";
+  protected String AUTHOR_FIELD_VALIDATION_MESSAGE = "author must not be empty or null";
+  protected String TITLE_FIELD_VALIDATION_MESSAGE = "title must not be empty or null";
+  protected String NUMBER_PAGES_FIELD_MIN_VALUE_VALIDATION_MESSAGE = "numberPages must not be 0";
+  protected String NUMBER_PAGES_FIELD_MAX_VALUE_VALIDATION_MESSAGE =
+      "numberPages must not be greater than 1000";
+  protected String CONTENT_TYPE = "application/json;charset=UTF-8";
+
+  protected void writeAsJson(Object object) throws JsonProcessingException {
+    this.json = new ObjectMapper().writeValueAsString(object);
+  }
+
+  protected void onBookRegisterFailValidateMultipleMessages(
+      String firstValidationMessage, String secondValidationMessage) throws Exception {
+    ResultActions result =
+        mockMvc.perform(
+            MockMvcRequestBuilders.post(BOOK_API.concat(CREATE_BOOK))
+                .contentType(CONTENT_TYPE)
+                .content(json)
+                .accept(MediaType.APPLICATION_JSON));
+
+    result.andDo(print()).andExpect(status().isBadRequest());
+    String exceptionMessage =
+        Objects.requireNonNull(result.andReturn().getResolvedException()).getMessage();
+    assertTrue(
+        Stream.of(firstValidationMessage, secondValidationMessage)
+            .allMatch(exceptionMessage::contains));
+  }
+}
